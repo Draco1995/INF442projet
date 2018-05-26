631d78274ead7f4cc737efa538832dec2d36100d
TeTe <sun_te@hotmail.com>
2018-05-26 18:17:26 +0200

    201805261817

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,34 +1,134 @@
 package client;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.*;
 import fr.ratp.wsiv.GetLines;
 import fr.ratp.wsiv.Wsiv;
 import fr.ratp.wsiv.WsivPortType;
 import fr.ratp.wsiv.xsd.Direction;
+import fr.ratp.wsiv.xsd.GeoPoint;
 import fr.ratp.wsiv.xsd.Line;
 import fr.ratp.wsiv.xsd.Mission;
 import fr.ratp.wsiv.xsd.ObjectFactory;
+import fr.ratp.wsiv.xsd.Station;
 import fr.ratp.wsiv.xsd.WrMission;
 import fr.ratp.wsiv.xsd.WrMissions;
 import fr.ratp.wsiv.xsd.WrStations;
-import fr.ratp.wsiv.xsd.Station;
-import java.util.List;
 public class Client {
 	
 	public static void main(String[] args) {
-		Line l = new Line();
 		ObjectFactory of = new ObjectFactory();
-		of.createLineId("RA");
-		//l.setCodeStif(of.createLineCodeStif("18-0080"));
-	//	l.setCode(of.createLineCode("18-0080"));
-		l.setId(of.createLineId("M1"));
-		//TODO l.setCodeStif(of.createLineCodeStif("1800001"));
 		Wsiv ws = new Wsiv();
 		WsivPortType wpt = ws.getWsivSOAP11PortHttp();
-		Station s=of.createStation();
-		s.setLine(of.createStationLine(l));
-		List <Station> stations =wpt.getStations(s, null,null,null,null).getStations();  
-		for(Station station: stations) {
-			System.out.println(station.getName().getValue());
+		Line line=of.createLine();
+		line.setId(of.createLineId("RB"));
+		//List<Line> lines=wpt.getLines(line);
+		//getAllLines gal=new getAllLines(wpt,of);
+		List<Mission> missions=get.getMN(3,"RB", "LOZERE", "R", of, wpt);
+		Station station=of.createStation();
+		station.setLine(of.createStationLine(line));
+		Direction dir=of.createDirection();
+		dir.setSens(of.createDirectionSens("*"));
+		//List<Mission> missions =wpt.getMissionsNext(station, dir, dateStart, limit)
+		WrMissions mm=wpt.getMissionsFirstLast(station, dir, "201805262300");
+		//mm.get
+		
+		for(Mission m: mm.getMissions()) {
+			System.out.println(m.getStationsDates().get(0));
+			try {
+				
+				System.out.println(m.getStationsMessages().get(0));
+			}catch(Exception e) {}
+			
 		}
+
+		return ;
+
+		
 	}
 }
+
+
+/*
+ * 		GeoPoint gp =of.createGeoPoint();
+		
+		gp.setX(595260.0);
+		gp.setY(2431446.0);
+		List<Integer> distances=new ArrayList<Integer>();
+		distances.add(50000);
+		//distances.add(50000);
+		Station station=of.createStation();
+		station.setName(of.createStationName("LOZERE"));
+		//station.setGeoPointA(of.createItineraryGeoPointEnd(of.createGeoPoint()));
+		List<Integer>d=new ArrayList<Integer>();
+		d.add(10000);
+		Direction direction =
+				
+				of.createDirection();
+		direction.setSens(of.createDirectionSens("A"));
+		direction.setLine(of.createDirectionLine(line));
+		//WrStations stations =wpt.getStations(null, gp, null, null, null);
+		wpt.getMissionsFirstLast(station, null, null);
+		WrMissions wrm=wpt.getMissionsFrequency(station, direction, null, null, null,null);
+		
+     	System.out.println(wrm.getMissions().get(0).getStationsDates().get(0));
+     	//wpt.getMissionsFrequency(station, direction, stationEndLine, stationEnd, datesStart, datesEnd)
+ */
+
+
+
+/*
+try {
+	PrintStream out =new PrintStream (new FileOutputStream("allLine.txt"));
+	
+	for(Line l:lines) {
+		if(l.getReseau().getValue().getName().getValue().contains("Bus")
+			||l.getReseau().getValue().getName().getValue().equals("M¨¦tro")||
+				l.getReseau().getValue().getName().getValue().equals("Tramway")) {
+			out.println(l);
+			
+		}
+		
+	}
+	out.close();
+}catch (FileNotFoundException e) {
+	e.printStackTrace();
+}
+System.out.println("InfoCatched");
+*/
+
+//wpt.getStations(station, gp, distances, limit, sortAlpha)
+
+/*
+Line l = new Line();
+ObjectFactory of = new ObjectFactory();
+l.setId(of.createLineId("RB"));
+//TODO 
+l.setCodeStif(of.createLineCodeStif("18-0080"));
+l.setCode(of.createLineCode("18-0080"));
+Wsiv ws = new Wsiv();
+WsivPortType wpt = ws.getWsivSOAP11PortHttp();
+of.createStation();
+
+
+
+Station s=of.createStation();
+//s.setName(of.createStationName("LOZERE"));
+//s.setLine(of.createStationLine(l));
+
+WrStations getStation=wpt.getStations(s, null, null, null, null);
+System.out.println(getStation.getStations().size());
+for(Station station : getStation.getStations()) {
+	System.out.println("Station: " +station.getName().getValue());
+}
+/*
+Direction dir=of.createDirection();
+dir.setSens(of.createDirectionSens("A"));
+for(Mission m: wpt.getMissionsNext(s, dir, null, null).getMissions()) {
+	System.out.println(m.getStations());
+}
+//System.out.println(wpt.getMissionsNext(s, dir, null, null).getMissions().get(0).getStationsDates().get(0));
+//System.out.println(wpt.getStations(s, null, null, null, null).getStations().get(0).getId().getValue());
+*/
--- src/getting/Stops.java
+++ src/getting/Stops.java
@@ -1,5 +1,8 @@
 package getting;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,23 +19,42 @@ public class Stops {
 		WsivPortType wpt = ws.getWsivSOAP11PortHttp();
 		
 		Line line =of.createLine();
-		line.setId(of.createLineId("M1"));
+		//line.setId(of.createLineId("RB"));
+	
+		
+		List <Line> lines=wpt.getLines(line);
+		try {
+			PrintStream out =new PrintStream (new FileOutputStream("allLine.txt"));
+			
+			for(Line l:lines) {
+				
+					out.println(l);
+					
+				
+				
+			}
+			out.close();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		
 		Station station =of.createStation();
 		station.setLine(of.createStationLine(line));
-		station.setName(of.createStationName("LA DEFENSE"));
+		station.setName(of.createStationName("LOZERE"));
 		Direction direction =of.createDirection();
 		direction.setSens(of.createDirectionSens("A"));
 		
 		List < Mission > missions =wpt.getMissionsNext(station, direction,null, null).getMissions();
 		for(Mission m: missions) {
-			System.out.println(m.getStationsDates().get(0));
+			System.out.println(m.getStations().get(1));
+			System.out.println(m.getStationsMessages().get(0));
 			System.out.println(m.getId().getValue());
 			System.out.println("code: "+m.getCode().getValue());
 		}
 		
 		
-		
+		/*
 		Mission mission=of.createMission();
 		mission.setId(of.createMissionId("PINS"));
 		
@@ -41,7 +63,7 @@ public class Stops {
 		WrMission newM=wpt.getMission(mission, null, null, null);
 		System.out.println(newM.getMission().getValue().getStationsDates().get(0));
 		
-		
+		*/
 		return;
 	}
 	/**
@@ -64,6 +86,44 @@ public class Stops {
 			stopsname.add(s.getName().getValue());
 		}
 		return stopsname;
+	}
+	public static void get() {
+		ObjectFactory of = new ObjectFactory();
+		Wsiv ws = new Wsiv();
+		WsivPortType wpt = ws.getWsivSOAP11PortHttp();
+		Station station=of.createStation();
+		Line RB =of.createLine();
+		RB.setId(of.createLineId("RB"));
+		station.setLine(of.createStationLine(RB));
+		List <Station> stationsRB=wpt.getStations(station, null, null, null, null).getStations();
+		
+		for(Station s : stationsRB) {
+			Direction direction =of.createDirection();
+			direction.setLine(of.createDirectionLine(RB));
+			direction.setSens(of.createDirectionSens("*"));
+			wpt.getMissionsNext(s, direction, null, null).getMissions();
+			/*
+			try {
+				PrintStream out =new PrintStream (new FileOutputStream("allLine.txt"));
+				
+				for(Line l:lines) {
+					if(l.getReseau().getValue().getName().getValue().contains("Bus")
+						||l.getReseau().getValue().getName().getValue().equals("M¨¦tro")||
+							l.getReseau().getValue().getName().getValue().equals("Tramway")) {
+						out.println(l);
+						
+					}
+					
+				}
+				out.close();
+			}catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}*/
+			System.out.println("InfoCatched");
+			
+			
+		}
+		
 	}
 
 	
