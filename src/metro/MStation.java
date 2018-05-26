c7a36308c407c27a8d23dd094996a507339a6238
Draco1995 <349084298@qq.com>
2018-05-26 15:25:02 +0200

    1
    1

--- src/metro/MStation.java
+++ src/metro/MStation.java
@@ -1,5 +1,17 @@
 package metro;
 
 public class MStation {
-
+	private String name;
+	MStation (String name){
+		this.name = name;
+	}
+	
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
 }
