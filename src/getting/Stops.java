c252270dbe288d6d9d6bab617513f7f7ca1e0295
TeTe <sun_te@hotmail.com>
2018-05-26 21:28:10 +0200

    201805262128

--- src/getting/Stops.java
+++ src/getting/Stops.java
@@ -3,9 +3,13 @@ package getting;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import fr.ratp.wsiv.Wsiv;
 import fr.ratp.wsiv.WsivPortType;
 import fr.ratp.wsiv.xsd.ObjectFactory;
@@ -21,24 +25,8 @@ public class Stops {
 		Line line =of.createLine();
 		//line.setId(of.createLineId("RB"));
 	
-		
-		List <Line> lines=wpt.getLines(line);
-		try {
-			PrintStream out =new PrintStream (new FileOutputStream("allLine.txt"));
-			
-			for(Line l:lines) {
-				
-					out.println(l);
-					
-				
-				
-			}
-			out.close();
-		}catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		
+		get();
+		/*
 		Station station =of.createStation();
 		station.setLine(of.createStationLine(line));
 		station.setName(of.createStationName("LOZERE"));
@@ -52,7 +40,7 @@ public class Stops {
 			System.out.println(m.getId().getValue());
 			System.out.println("code: "+m.getCode().getValue());
 		}
-		
+		*/
 		
 		/*
 		Mission mission=of.createMission();
@@ -87,7 +75,43 @@ public class Stops {
 		}
 		return stopsname;
 	}
+	
+	public static List <Station> getAllStations(String linename,ObjectFactory of,WsivPortType wpt){
+		Station station=of.createStation();
+		Line RB =of.createLine();
+		RB.setId(of.createLineId(linename));
+		station.setLine(of.createStationLine(RB));
+		List <Station> stationsRB=wpt.getStations(station, null, null, null, null).getStations();
+		return stationsRB;
+		
+	}
+	
+	public static List < Mission> getAllMission(Station station, String dir,ObjectFactory of,WsivPortType wpt){
+		Direction direction =of.createDirection();
+		direction.setLine(of.createDirectionLine(station.getLine().getValue()));
+		direction.setSens(of.createDirectionSens(dir));
+		return wpt.getMissionsNext(station, direction, null, null).getMissions();
+	}
+	
+	public static String InfoLine(Mission mission) {
+		String s="";
+		s+=mission.getStations().get(0).getName().getValue()+'\t';
+		
+		Date date=new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+		String now = dateFormat.format( date );
+		//out.print(now+'\t');
+		s+=now+'\t';
+		
+		for(String time : mission.getStationsDates()) {
+			s+=time+'\t';
+		}
+		return s;
+	}
+	
+	
 	public static void get() {
+		int getTime=0;
 		ObjectFactory of = new ObjectFactory();
 		Wsiv ws = new Wsiv();
 		WsivPortType wpt = ws.getWsivSOAP11PortHttp();
@@ -96,33 +120,60 @@ public class Stops {
 		RB.setId(of.createLineId("RB"));
 		station.setLine(of.createStationLine(RB));
 		List <Station> stationsRB=wpt.getStations(station, null, null, null, null).getStations();
+		getTime++;
 		
-		for(Station s : stationsRB) {
-			Direction direction =of.createDirection();
-			direction.setLine(of.createDirectionLine(RB));
-			direction.setSens(of.createDirectionSens("*"));
-			wpt.getMissionsNext(s, direction, null, null).getMissions();
-			/*
-			try {
-				PrintStream out =new PrintStream (new FileOutputStream("allLine.txt"));
+		try {
+			PrintStream out =new PrintStream (new FileOutputStream("allStation.txt"));
+			Runnable runnable =new Runnable() {
+				public void run() {
+			
+			for(Station s : stationsRB) {
 				
-				for(Line l:lines) {
-					if(l.getReseau().getValue().getName().getValue().contains("Bus")
-						||l.getReseau().getValue().getName().getValue().equals("M¨¦tro")||
-							l.getReseau().getValue().getName().getValue().equals("Tramway")) {
-						out.println(l);
-						
-					}
+				
+				out.print(s.getName().getValue()+'\t');
+				Date date=new Date();
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+				String now = dateFormat.format( date );
+				out.print(now+'\t');
+				Direction direction =of.createDirection();
+				direction.setLine(of.createDirectionLine(RB));
+				direction.setSens(of.createDirectionSens("*"));
+				List < Mission> missions = wpt.getMissionsNext(s, direction, null, null).getMissions();
+				//getTime++;
+				//out.print("number: "+missions.size()+'\t');
+				for(Mission m: missions) {
 					
+					List <String> nextTime=m.getStationsDates();
+					
+					for(String i: nextTime) {
+						out.print(i+'\t');
+					}
+					List <String> nextMessage=m.getStationsMessages();
+					for(String i: nextMessage) {
+						out.print(i+'\t');
+					}
 				}
-				out.close();
-			}catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}*/
-			System.out.println("InfoCatched");
-			
-			
+				
+				out.println();
+				
+			}
+			Date date=new Date();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+			String now = dateFormat.format( date );
+			System.out.println(now);
+				}};
+				
+		    ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();  	
+		    service.scheduleAtFixedRate(runnable, 0, 10, TimeUnit.SECONDS);  
+		    out.close();	
+		    
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+		System.out.println("Finished "+getTime);
+		
+			
+			
 		
 	}
 
